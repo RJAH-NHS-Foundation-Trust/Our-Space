14ef21c1f3419008721a7026978b93187c442eeb
Howell <r.howell@nhs.net>
2024-02-19 09:15:18 +0000

    Update page-recipe.php
    Loop to collect all recipes and display on the page.

--- src/page-recipe.php
+++ src/page-recipe.php
@@ -10,31 +10,47 @@
   
 ?>
 
+<div class="container mt-4">
+    
+    <div class="row mb-4">
+        <?php if( function_exists( 'aioseo_breadcrumbs' ) ) aioseo_breadcrumbs(); ?>
+    </div>
+    
+    <h2><?php the_title(); ?></h2>
+    <p><?php the_content(); ?> 
+</div>
+
+<?php          
+    $args = array (
+        'post_status' => 'publish',
+        'post_type' => 'recipe',
+        // 'tax_query' => array(
+        //   array(
+        //     'taxonomy' => 'diet', // Your custom taxonomy slug
+        //     'field' => 'slug',
+        //     'terms' => 'vegetarian', // Slug of the diet you want to filter by
+        //   ),
+        // ),
+        'orderby' => 'meta_value_num',
+        'order' => 'DESC',   
+    );
+
+    $posts = new WP_Query( $args );
+    $totalPopularPosts = $posts -> found_posts;
+
+if($totalPopularPosts > 0) { ?>
+
 <section class="main-section mt-4">
     <div class="container">
-      <div class="row">        
-        <div class="col-lg-9 col-md-12 blog-posts">
-        <?php          
-            $args = array (
-                'post_status' => 'publish',
-                'post_type' => 'recipe',
-                'order'          => 'desc',
-                'orderby'        => 'publish_date',    
-            );
-
-            $posts = new WP_Query( $args );
-            $totalPopularPosts = $posts -> found_posts;
-            
-            if ( $posts->have_posts() ) :  while ( $posts->have_posts() ) : $posts->the_post(); 
+        <div class="row">
+            <?php if ( $posts->have_posts() ) :  while ( $posts->have_posts() ) : $posts->the_post(); 
                 get_template_part( 'recipe-content', get_post_format() );
                     endwhile; endif; wp_reset_postdata(); 
         ?>
-            
-            
-        </div>
-        <?php get_sidebar() ?>
       </div>
     </div>
   </section>
+
+  <?php } ?>
 
   <?php get_footer(); ?>
