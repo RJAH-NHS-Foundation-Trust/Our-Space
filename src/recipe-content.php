144695c0b984527c74866667625562c44839664a
Howell <r.howell@nhs.net>
2024-02-19 09:15:03 +0000

    Update recipe-content.php
    Built template for recipe page.

--- src/recipe-content.php
+++ src/recipe-content.php
@@ -1,9 +1,41 @@
-<div class="recipe-card d-flex flex-column">
-    <div class="recipe-image">
-        <img class="rounded" src="<?php echo get_bloginfo('template_directory'); ?>/img/food.webp" alt="Recipe Image">
-    </div>
+<div class="col-md-4 blog-post mr-2">
+    <h2 class="blog-title"><a class="blog-title" href="<?php the_permalink() ?>" rel="bookmark" title="Permanent Link to <?php the_title_attribute(); ?>"><?php the_title(); ?></a></h2>
+    <div class="blog-meta">        
+        <div class="mb-2 mt-2">
+            <?php
+                $post_id = get_the_ID();
+                $categories = get_the_category($post_id);
 
-        <h3 class="recipe-title mt-2"><a href="<?php the_permalink() ?>" rel="bookmark" title="Permanent Link to <?php the_title_attribute(); ?>"><?php the_title(); ?></a></h2>
-        <p class="recipe-description"><?php the_excerpt(); ?></p>
-        <a href="<?php the_permalink(); ?>" class="btn btn-primary">View Recipe</a>        
+                foreach ($categories as $cat) {
+                    $category_link = get_category_link($cat->cat_ID);
+                    echo '<a  class="custom-category-pill" href="' . esc_url($category_link) . '" title="' . esc_attr($cat->name) . '">' . esc_html($cat->name) . '</a> ';
+                }
+                ?>
+            </span>
+        </div>
+    </div>
+    <div class="blog-excerpt">
+        <?php if(has_post_thumbnail()) { ?>
+        <img src="<?php the_post_thumbnail_url() ?>" alt="Thumbnail" class="thumbnail">
+        <?php } ?>
+        <p><?php the_excerpt(); ?></p>
+        <a href="<?php the_permalink(); ?>" class="btn btn-outline-success">Read More</a>
+    </div>
+    <?php
+        $posttags = get_the_tags(); 
+        if ($posttags) 
+            {
+    ?>
+    <div class="blog-tags">        
+    <?php                              
+        foreach($posttags as $tag) 
+        { ?>
+            <span class="tag">
+            <?php echo '<a href="' . get_tag_link($tag->term_id) . '">' . $tag->name . '</a>'; ?>
+            </span>
+
+    <?php } ?>
+
+    </div>
+    <?php } ?>
 </div>
