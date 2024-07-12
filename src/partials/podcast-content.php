1d70349637aef0b01f0b3d9b932c44a14ebfd517
Howell <r.howell@nhs.net>
2024-07-12 15:13:26 +0100

    Update podcast-content.php
    Added checks for empty custom fields to no longer display items.

--- src/partials/podcast-content.php
+++ src/partials/podcast-content.php
@@ -2,13 +2,22 @@
     <h2 class="blog-title"><a class="blog-title" href="<?php the_permalink() ?>" rel="bookmark" title="Permanent Link to <?php the_title_attribute(); ?>"><?php the_title(); ?></a></h2>
     <?php get_template_part( 'meta-content') ?>
     <div class="blog-excerpt">
-        <img src="<?php the_field('podcast_art'); ?>" alt="Thumbnail" class="thumbnail">
+        <?php $podcast_art = get_field('podcast_art'); ?>
+        <?php if(!empty($podcast_art)) { ?>
+        <img src="<?php the_field('podcast_art'); ?>" alt="Thumbnail" class="thumbnail"> 
+        <?php } ?>
         <p><?php the_excerpt(); ?></p>
         <div class="row mb-1">
+            <?php $more_info_link = get_field('podcast_page_link'); ?>
+            <?php if(!empty($more_info_link)) { ?>
             <a href="<?php the_field('podcast_page_link'); ?>" class="btn btn-outline-success">Find Out More</a>
+            <?php } ?>
         </div>
         <div class="row mb-1">
+            <?php $subscribe_link = get_field('podcast_subscribe_url'); ?>
+            <?php if(!empty($podcast_art)) { ?>
             <a href="<?php echo get_field('podcast_subscribe_url'); ?>" class="btn btn-outline-success">Subscribe</a>
+            <?php } ?>
         </div>
     </div>
     <?php
