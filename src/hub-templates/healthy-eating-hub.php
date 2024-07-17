438b50bf9262e35dc35bbae4ba0efbb3c67aecf5
Howell <r.howell@nhs.net>
2024-07-17 10:46:26 +0100

    Update healthy-eating-hub.php
    Added in filterable options
    Added pictures
    No post text changed to reflect page content

--- src/hub-templates/healthy-eating-hub.php
+++ src/hub-templates/healthy-eating-hub.php
@@ -24,24 +24,52 @@
     );
 
     $posts = new WP_Query( $args );
-    $totalPopularPosts = $posts -> found_posts;
+    $totalPopularPosts = $posts -> found_posts; ?>
 
-if($totalPopularPosts > 0) { ?>
+<section class="mt-4">
+  <div class="container">
+    <div class="row">
+      <div class="col-4">
+        <img class="img-fluid" src="<?php echo get_bloginfo('template_directory'); ?>/img/nhs_chef1.webp">
+      </div>
+      <div class="col-4">
+        <img class="img-fluid" src="<?php echo get_bloginfo('template_directory'); ?>/img/nhs_chef2.webp">
+      </div>
+      <div class="col-4">
+        <img class="img-fluid" src="<?php echo get_bloginfo('template_directory'); ?>/img/nhs_chef3.webp">
+      </div>
+    </div>
+  </div>
+</section>
 
-<section class="main-section mt-4">
+<?php if($totalPopularPosts > 0) { ?>
+
+  <section class="mt-4">
+  <div class="container">
+    <h3>Filter Your Selection</h3>
+    <div class="d-flex gap-2">
+      <a class="btn btn-primary" href="<?php echo get_option('home'); ?>/diet/vegan/">Vegan</a>
+      <a class="btn btn-primary" href="<?php echo get_option('home'); ?>/diet/vegetarian/">Vegetarian</a>
+      <a class="btn btn-primary" href="<?php echo get_option('home'); ?>/diet/gluten-free/">Gluten Free</a>
+    </div>
+  </div>
+</section>
+
+
+<section class="main-section mt-4 mb-4">
     <div class="container">
-        <div class="row g-grid gap-2 w-100 d-flex">
+        <div class="g-grid gap-2 w-100 d-flex">
             <?php if ( $posts->have_posts() ) :  while ( $posts->have_posts() ) : $posts->the_post(); 
                 get_template_part( 'partials/food-content', get_post_format() );
                     endwhile; endif; wp_reset_postdata(); 
-        ?>
-      </div>
+            ?>
+        </div>
     </div>
   </section>
 
   <?php } else { ?>
 
-    <p>Nothing to show</p>
+    <p>There are no recipes to display.</p>
 
   <?php } ?> 
 
