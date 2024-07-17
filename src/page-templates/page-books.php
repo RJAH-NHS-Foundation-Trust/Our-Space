82843c7a8246a8322072d4d794d01f2ad8ab88e4
Howell <r.howell@nhs.net>
2024-07-17 10:43:38 +0100

    Update page-books.php
    Fixes to the page layout.
    Added in additional link to library link

--- src/page-templates/page-books.php
+++ src/page-templates/page-books.php
@@ -13,7 +13,14 @@
 <div class="container mt-4">
     
     <h2><?php the_title(); ?></h2>
-    <p><?php the_content(); ?></p> 
+
+</div>
+
+    <section>
+        <div class="container">
+            <p><?php the_content(); ?></p> 
+        </div>
+    </section>
 
     <?php $paged = (get_query_var('paged')) ? get_query_var('paged') : ((get_query_var('page')) ? get_query_var('page') : 1);      
     $args = array (
@@ -28,25 +35,39 @@
     );
 
     $posts = new WP_Query( $args );
-    $totalPopularPosts = $posts -> found_posts;
+    $totalPopularPosts = $posts -> found_posts;?>
 
-if($totalPopularPosts > 0) { ?>
 
-<section class="main-section mt-4">
-    <div class="container">
-        <div class="row g-grid gap-2 w-100 d-flex">
-            <?php if ( $posts->have_posts() ) :  while ( $posts->have_posts() ) : $posts->the_post(); 
-                get_template_part( 'partials/book-content', get_post_format() );
-                    endwhile; ?> </div>
-                    <?php bootstrap_pagination($posts);
-                endif; wp_reset_postdata();?>
-    
-      </div>
-    </div>
-  </section>
+    <section>
+        <div class="container">
+        <div class="row mt-4">
+                <div class="col-md-12">
+                    <div class="card">
+                        <div class="card-body neuro-card">
+                            <p class="card-text text-white">The RJAH Library has a large collection of books covering a large range of topics, you can view all of the books they have in their own catalogue which can be found by clicking the link below.</p>
+                            <a class="btn btn-primary" target="_blank" href="https://rjah.koha-ptfs.co.uk/cgi-bin/koha/opac-shelves.pl?op=view&shelfnumber=1541&sortfield=title">RJAH Wellbeing Collection</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            
+        </div>
+    </section>
 
-  <?php } ?>
+    <?php if($totalPopularPosts > 0) { ?>
 
-</div>
+    <section class="main-section mt-4">
+        <div class="container">
+            <div class="row gap-2 w-100 d-flex justify-content-center">
+                <?php if ( $posts->have_posts() ) :  while ( $posts->have_posts() ) : $posts->the_post(); 
+                    get_template_part( 'partials/book-content', get_post_format() );
+                endwhile; ?> 
+            </div>
+            <?php bootstrap_pagination($posts);
+            endif; wp_reset_postdata(); ?>
+        </div>
+    </section>
+
+    <?php } ?>
 
   <?php get_footer(); ?>
