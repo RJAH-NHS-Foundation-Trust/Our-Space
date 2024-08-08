8213ee26bf1f271087fb47ac4f55622efd88d3d3
Howell <r.howell@nhs.net>
2024-08-08 13:32:28 +0100

    MHFA Supervision Page Template
    Draft page created

---
+++ src/page-templates/page-mhfa-support.php
@@ -0,0 +1,122 @@
+<?php 
+
+    /** 
+     * Template Name: MHFA Supervision Page Template
+     * 
+     * 
+    */
+
+    get_header(); 
+  
+?>
+
+<header class="neuro-header text-center text-md-left">
+    <div class="container-fluid">
+        <div class="row neuro-header-content align-items-center">
+            <div class="col-12">
+                <!-- <img class="img-fluid" src="<?php echo get_bloginfo('template_directory'); ?>/img/neurodiversity-rainbow.webp"> -->
+                <h1 class="mt-4">Mental Health First Aider</h1>
+                <h3>Supervision Sessions</h3>
+            </div>
+        </div>
+    </div>
+</header>
+
+<section class="neuro-section">
+    <div class="container text-center">
+        <h2>What are supervision sessions?</h2>
+        <div class="row mt-4">
+            <div class="col-md-12">
+                <div class="card neuro-card">
+                    <div class="card-body text-white">
+                        <p class="card-text">Some words can go here explaining what these sessions are about. Some words can go here explaining what these sessions are about. Some words can go here explaining what these sessions are about. Some words can go here explaining what these sessions are about. Some words can go here explaining what these sessions are about. Some words can go here explaining what these sessions are about. Some words can go here explaining what these sessions are about. Some words can go here explaining what these sessions are about. Some words can go here explaining what these sessions are about. </p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+
+<section class="neuro-section">
+    <div class="container text-center">
+        <h2>Upcoming Sessions</h2>
+        <div class="row mt-4">
+            <div class="col-md-4 mb-4">
+                <div class="card neuro-card">
+                    <div class="card-body text-white">
+                        <h2 class="card-title">July 1st 2024</h2>
+                        <p class="card-text"><span class="fw-bold">Location:</span> Some Location</p>
+                        <p class="card-text"><span class="fw-bold">Time:</span> 00:00 - 00:00</p>
+                        <p class="card-text">A short description about the meeting.</p>                        
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-4 mb-4">
+                <div class="card neuro-card">
+                    <div class="card-body text-white">
+                        <h2 class="card-title">July 1st 2024</h2>
+                        <p class="card-text"><span class="fw-bold">Location:</span> Some Location</p>
+                        <p class="card-text"><span class="fw-bold">Time:</span> 00:00 - 00:00</p>
+                        <p class="card-text">A short description about the meeting.</p>                        
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-4 mb-4">
+                <div class="card neuro-card">
+                    <div class="card-body text-white">
+                        <h2 class="card-title">July 1st 2024</h2>
+                        <p class="card-text"><span class="fw-bold">Location:</span> Some Location</p>
+                        <p class="card-text"><span class="fw-bold">Time:</span> 00:00 - 00:00</p>
+                        <p class="card-text">A short description about the meeting.</p>                        
+                    </div>
+                </div>
+            </div>
+        </div>
+            <div class="row mt-2">
+            <div class="col-md-4 mb-4">
+                <div class="card neuro-card">
+                    <div class="card-body text-white">
+                        <h2 class="card-title">July 1st 2024</h2>
+                        <p class="card-text"><span class="fw-bold">Location:</span> Some Location</p>
+                        <p class="card-text"><span class="fw-bold">Time:</span> 00:00 - 00:00</p>
+                        <p class="card-text">A short description about the meeting.</p>                        
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-4 mb-4">
+                <div class="card neuro-card">
+                    <div class="card-body text-white">
+                        <h2 class="card-title">July 1st 2024</h2>
+                        <p class="card-text"><span class="fw-bold">Location:</span> Some Location</p>
+                        <p class="card-text"><span class="fw-bold">Time:</span> 00:00 - 00:00</p>
+                        <p class="card-text">A short description about the meeting.</p>                        
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-4 mb-4">
+                <div class="card neuro-card">
+                    <div class="card-body text-white">
+                        <h2 class="card-title">July 1st 2024</h2>
+                        <p class="card-text"><span class="fw-bold">Location:</span> Some Location</p>
+                        <p class="card-text"><span class="fw-bold">Time:</span> 00:00 - 00:00</p>
+                        <p class="card-text">A short description about the meeting.</p>                        
+                    </div>
+                </div>
+            </div>
+        </div>
+            <div class="row mt-2">
+            <div class="col-md-4 mb-4">
+                <div class="card neuro-card">
+                    <div class="card-body text-white">
+                        <h2 class="card-title">July 1st 2024</h2>
+                        <p class="card-text"><span class="fw-bold">Location:</span> Some Location</p>
+                        <p class="card-text"><span class="fw-bold">Time:</span> 00:00 - 00:00</p>
+                        <p class="card-text">A short description about the meeting.</p>                        
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+
+<?php get_footer(); ?>
