67255c3a302c650396bed07f806b679beecf2ae9
Howell <r.howell@nhs.net>
2024-08-08 13:30:45 +0100

    Library links
    Added library links and logo

--- src/page-templates/page-books.php
+++ src/page-templates/page-books.php
@@ -39,20 +39,22 @@
 
 
     <section>
-        <div class="container">
-        <div class="row mt-4">
-                <div class="col-md-12">
-                    <div class="card">
-                        <div class="card-body neuro-card">
-                            <p class="card-text text-white">The RJAH Library has a large collection of books covering a large range of topics, you can view all of the books they have in their own catalogue which can be found by clicking the link below.</p>
-                            <a class="btn btn-primary" target="_blank" href="https://rjah.koha-ptfs.co.uk/cgi-bin/koha/opac-shelves.pl?op=view&shelfnumber=1541&sortfield=title">RJAH Wellbeing Collection</a>
+            <div class="container">
+                <div class="row mt-4">
+                    <div class="col-md-12">
+                        <div class="card">
+                            <div class="card-body neuro-card d-flex align-items-center">
+                                <img style="width:100px" class="me-3" src="<?php echo get_bloginfo('template_directory'); ?>/img/library-logo.webp">
+                                <div>
+                                    <p class="card-text text-white">The RJAH Library has a large collection of books covering a large range of topics, you can view all of the books they have in their own catalogue which can be found by clicking the link below.</p>
+                                    <a class="btn btn-primary" target="_blank" href="https://rjah.koha-ptfs.co.uk/cgi-bin/koha/opac-shelves.pl?op=view&shelfnumber=1541&sortfield=title">RJAH Wellbeing Collection</a>
+                                </div>
+                            </div>
                         </div>
                     </div>
-                </div>
+                </div>            
             </div>
-            
-        </div>
-    </section>
+        </section>
 
     <?php if($totalPopularPosts > 0) { ?>
 
