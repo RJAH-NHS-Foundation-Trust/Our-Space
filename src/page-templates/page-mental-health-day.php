4f06d8f65cb94fc419025c28d104b6e6dc5009f8
Howell <r.howell@nhs.net>
2024-10-07 08:28:37 +0100

    20241004 Changes

---
+++ src/page-templates/page-mental-health-day.php
@@ -0,0 +1,329 @@
+<?php 
+
+    /** 
+     * Template Name: WMHD Page Template
+     * 
+     * 
+    */
+
+    get_header(); 
+  
+?>
+
+<section class="neuro-section">
+    <div class="container text-center">
+        <div class="row neuro-header-content align-items-center">
+            <h1>World Mental Health Day 2024</h1>
+            <div class="col-6">                
+                <p class="text-dark">On World Mental Health Day, let’s all come together to talk about mental health and show everyone that mental health matters. Talking about our mental health can help us cope better with life’s ups and downs. So, on World Mental Health Day and beyond it, why not check in with your friends, family, peers, or colleagues? </p>                
+                <p class="text-dark">World Mental Health Day is celebrated every year on 10 October. This year’s theme set by the World Federation of Mental Health is workplace mental health. <strong>The theme highlights the importance of addressing mental health and wellbeing in the workplace</strong>, for the benefit of people, organisations, and communities. </p>                
+            </div>
+            <div class="col-6">
+                <img style="max-height:300px" src="<?php echo get_bloginfo('template_directory'); ?>/img/WMHD24.jpg" alt="World Mental Health Day Logo">
+            </div>
+        </div>
+    </div>
+</section>
+
+<section class="neuro-section">
+    <div class="container text-center">  
+        <h2>Top tips to look after your mental health</h2>
+        <p>Here are some ways to stay mentally well</p>
+        <div class="row mt-2">
+            <div class="col-md-6 mb-4">
+                <div class="card mhd-card">
+                    <div class="card-body text-white">
+                        <h2 class="card-title">Talk about your feelings</h2>
+                        <ul class="list-unstyled">
+                        <li class="card-text">Just being listened to can help you feel supported and less alone.</li>                        
+                        </ul>
+                        <a class="btn btn-mhd" href="<?php echo get_option('home'); ?>/mental-health-first-aiders">Contact a mental health first aider</a>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-6">
+                <div class="card mhd-card-alt">
+                    <div class="card-body">
+                        <h2 class="card-title">Stay active</h2>
+                        <ul class="list-unstyled">
+                            <li class="card-text">Physical activity is not only good for your body, but it’s also great for your mind.</li>                        
+                        </ul>
+                        <a class="btn btn-mhd" href="<?php echo get_option('home'); ?>/personal-fitness">Find an exercise that suits you</a>
+                    </div>
+                </div>
+            </div>
+            </div>
+            <div class="row mt-2">
+            <div class="col-md-6">
+                <div class="card mhd-card-alt">
+                    <div class="card-body">
+                        <h2 class="card-title">Get a good night’s sleep</h2>
+                        <ul class="list-unstyled">
+                        <li class="card-text">Sleep and mental health are closely linked</li>                        
+                        </ul>                  
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-6">
+                <div class="card mhd-card">
+                    <div class="card-body text-white">
+                        <h2 class="card-title">Practice mindfulness</h2>
+                        <ul class="list-unstyled">
+                        <li class="card-text">A way to be fully engaged and present in the moment..</li>
+                        </ul>
+                        <a class="btn btn-mhd" href="<?php echo get_option('home'); ?>/meditation">View Our Guided Meditations</a>
+                    </div>
+                </div>
+            </div>
+            </div>
+            <div class="row mt-2">
+            <div class="col-md-6">
+                <div class="card mhd-card">
+                    <div class="card-body text-white">
+                        <h2 class="card-title">Eat well </h2>
+                        <ul class="list-unstyled">
+                        <li class="card-text">A balanced diet can improve your sense of well-being and your mood.</li>
+                        </ul>  
+                        <a class="btn btn-mhd" href="<?php echo get_option('home'); ?>/healthy-eating">View Our Recipe Directory</a>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-6">
+                <div class="card mhd-card-alt">
+                    <div class="card-body">
+                        <h2 class="card-title">Keep in touch with friends and family</h2>
+                        <ul class="list-unstyled">
+                        <li class="card-text">This helps you deal with the stresses of life, makes you feel cared for and offers a different viewpoint.</li>
+                        </ul>                  
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <section id="tea-talk" class="neuro-section">
+        <div class="container text-center">           
+            <div class="row mt-4">
+                    <div class="col-md-12 mb-2">
+                        <div class="card mhd-card-alt">
+                            <div class="card-body">
+                            <h2 class="card-title">Tea & Talk</h2>                        
+                                <p class="card-text fw-bold">Pick an environment where you feel comfortable and where you can talk without distractions. Sometimes its easier to talk side by side rather than face-to-face, so perhaps you could go for a walk in the local park or have a chat while doing another activity such as making a cup of tea or cooking. </p>
+                                <a class="btn btn-mhd" href="https://forms.office.com/e/x6PZ3QCrS7" target="_blank">Give the quiz a go and potentially win a small prize</a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+
+        <section class="neuro-section">
+            <div class="container text-center">
+            <h2>Talking tips</h2>
+            <p class="text-dark">Talking to people about mental health can hugely benefit both parties, but we know that having these conversations, especially if you have never done so before, isn’t always easy. There is no right or wrong way to talk about mental health but we’ve created these tips to support you with those conversations. </p>
+            <div class="row mt-4">
+                    <div class="col-md-12 mb-2">
+                        <div class="card mhd-card">
+                            <div class="card-body text-white">
+                            <h2 class="card-title">Think about the time and place</h2>                        
+                                <p class="card-text">Pick an environment where you feel comfortable and where you can talk without distractions. Sometimes its easier to talk side by side rather than face-to-face, so perhaps you could go for a walk in the local park or have a chat while doing another activity such as making a cup of tea or cooking. </p>
+                                
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+
+        <div class="row mt-2">
+            <div class="col-md-6 mb-4">
+                <div class="card mhd-card-alt">
+                    <div class="card-body">
+                        <h2 class="card-title">Know your limits</h2>
+                        <ul class="list-unstyled">
+                        <li class="card-text">You will have your own limits on the support that you can provide and it’s important to take care of yourself too. Give yourself time to rest and process what they have told you or what’s happened. </li>
+                        <li class="card-text">Try to help them create a support network of other friends, relatives and mental health professionals who can help them too.</li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-6 mb-4">
+                <div class="card mhd-card">
+                    <div class="card-body text-white">
+                        <h2 class="card-title">Listen and ask questions</h2>
+                        <ul class="list-unstyled">
+                        <li class="card-text">Make sure you give your undivided attention and listen actively when someone is talking. Let them finish their sentences and thoughts without interrupting and encourage them to talk by asking open rather than leading questions’, for example you could say “Why don’t you tell me how you are feeling?” rather than “I can see you are feeling very low”. Try to keep your language neutral and give the person time to answer</li>
+
+                        </ul>
+                    </div>
+                </div>
+            </div>            
+        </div>
+        <div class="row mt-2">
+            <div class="col-md-6 mb-4">
+                <div class="card mhd-card">                    
+                    <div class="card-body text-white">
+                        <h2 class="card-title">Don’t change the way you treat them</h2>
+                        <ul class="list-unstyled">
+                        <li class="card-text">If someone opens up to you about a mental health problem it’s important to remember that they are the same person and won’t want to be treated any differently. Do what you usually do – behaving differently can make someone feel more isolated.</li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-6 mb-4">
+                <div class="card mhd-card-alt">                    
+                    <div class="card-body">
+                        <h2 class="card-title">Let them share as much as they want to</h2>
+                        <ul class="list-unstyled">
+                            <li class="card-text">Let them lead the discussion at their own pace and don’t pressure them to tell you anything they aren’t ready to talk about. Talking can take a lot of trust and courage and you might be the first person they have been able to talk to about this</li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="row mt-2">
+            <div class="col-12">
+                <div class="card mhd-card">                    
+                    <div class="card-body text-white">
+                        <h2 class="card-title">You don’t have to have all the answers</h2>
+                            <p>Try not to make assumptions about what is wrong or jump in too quickly with your own diagnosis or solutions. The important thing is to listen rather than feeling like you need to fix anything,as this isn’t always realistic and it can be more helpful to the other person if you just listen and offer your support, unless they’ve asked for advice directly.</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+
+<section class="neuro-section">
+    <div class="container text-center"> 
+        <div class="row">
+            <div class="col-12">
+                <div class="card neuro-card" style="background-color: #024638 !important">
+                    <div class="card-body text-white">
+                        <h2 class="card-title">Get help</h2>
+                        <p>If you or someone you know is struggling with their mental health, you are not alone, and there are places you can go to get help.</p>
+                        <a class="btn btn-mhd" href="<?php echo get_option('home'); ?>/crisis">Get Support</a>
+                    </div>
+                </div>
+            </div>
+        </div>  
+    </div>
+</section>
+
+
+<section class="neuro-section">
+    <div class="container text-center"> 
+        <h2>Icebreakers</h2>
+        <p class="text-dark">Below are some suggestions that you can use as ice-breakers in work meetings, or with friends and family to gently open up the conversation around mental health</p>
+        <div class="row mt-2">
+            <div class="col-md-6 mb-4">
+                <div class="card mhd-card">
+                    <div class="card-body text-white">
+                        <h2 class="card-title">Stretching</h2>
+                        <p>Lead a group stretch before you tuck into your cake, to help everyone feel calmer and boost serotonin levels.</p>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-6 mb-4">
+                <div class="card mhd-card-alt">
+                    <div class="card-body">
+                        <h2 class="card-title">Describe how you feel today using a colour</h2>
+                        <p>Colours help us express our feelings when we might not have the right words.</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="row mt-2">
+            <div class="col-md-6 mb-4">
+                <div class="card mhd-card-alt">
+                    <div class="card-body">
+                        <h2 class="card-title">Share nice feedback</h2>
+                        <p>Take a moment to write down something you admire abouta friend or a colleague and share this with them It’ll be guaranteed to make their day.</p>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-6 mb-4">
+                <div class="card mhd-card">
+                    <div class="card-body text-white">
+                    <h2 class="card-title">How are you feeling on a scale of 1–10? </h2>
+                        <p>With 1 being very low and 10 being fantastic, this is a good honesty exercise, and can help you reflect on what might be going on, especially if you’re feeling a little out of sync.</p>
+                    </div>
+                </div>
+            </div>            
+        </div>
+        <div class="row mt-2">
+            <div class="col-md-6 mb-4">
+                <div class="card mhd-card">
+                    <div class="card-body text-white">
+                        <h2 class="card-title">Breathing exercises</h2>
+                        <p>The NHS recommends the following breathing exercise for stress, anxiety and panic – it take just a few minutes and can be done anywhere. </p>
+                        <ol>
+                            <li class="card-text">Breathe in through your nose and count steadily from 1 to 5.</li>
+                            <li class="card-text">Breathe out through your nose, counting from 1 to 5 again</li>
+                            <li class="card-text">Repeat for 5 minutes. </li>
+                        </ol>
+                        <p>If this is your first time doing this exercise, you may not be able to get to 5 straight away, so just count up to whichever number is comfortable for you</p>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-6 mb-4">
+                <div class="card mhd-card-alt">
+                    <div class="card-body">
+                    <h2 class="card-title">Use an emoji to describe how you feel right now? </h2>
+                        <p>Again, this is a good way to express emotions without using words.</p>
+                        <p>You could even create a mood wall with different emojis and ask people to put their name against the emoticon that best displays their current state or mood.</p>
+                    </div>
+                </div>
+            </div>            
+        </div>
+    </div>
+</section>
+
+<section class="neuro-section">
+    <div class="container text-center">
+        <h2>Resources</h2>
+        <div class="row mt-2">
+            <div class="col-md-4 mb-4">
+                <div class="card mhd-card">                    
+                    <div class="card-body text-white">
+                        <h2 class="card-title">Mental Health First Aiders</h2>
+                        <a class="btn btn-mhd" target="_blank" href="<?php echo get_option('home'); ?>/mental-health-first-aiders">Find a Mental Health First Aider</a> 
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-4 mb-4">
+                <div class="card mhd-card">                    
+                    <div class="card-body text-white">
+                        <h2 class="card-title">Mental Health Resources</h2>
+                        <a class="btn btn-mhd" target="_blank" href="<?php echo get_option('home'); ?>/mental-health">View the resources.</a> 
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-4 mb-4">
+                <div class="card mhd-card">                    
+                    <div class="card-body text-white">
+                        <h2 class="card-title">Shropshire, Telford and Wrekin Staff Wellbeing Hub</h2>
+                        <a class="btn btn-mhd" target="_blank" href="<?php echo get_option('home'); ?>/shropshire-telford-and-wrekin-staff-wellbeing-hub">Contact the hub.</a> 
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="row mt-2">
+            <div class="col-md-4 mb-4">
+                <div class="card mhd-card">
+                    <div class="card-body text-white">
+                        <h2 class="card-title">Suicide Awareness Training (free)</h2>
+                        <a class="btn btn-mhd" target="_blank" href="https://www.zerosuicidealliance.com/suicide-awareness-training">Take the training</a> 
+                    </div>
+                </div>
+            </div>
+
+
+        </div>
+    </div>
+</section>
+
+<div class="container text-center">
+    <p>This page is built using resources from <a href="https://www.mentalhealth.org.uk/" target="_blank">Mental Health Foundation</a></p>
+</div>
+
+<?php get_footer(); ?>
