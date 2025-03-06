0b5a9ad7359e93e9f74aca04ab5a6d9c12f4bfc6
Rich Howell <r.howell@nhs.net>
2025-03-06 15:27:29 +0000

    Create page-conversation.php

---
+++ page-templates/page-conversation.php
@@ -0,0 +1,160 @@
+<?php 
+
+    /** 
+     * Template Name: Conversation Page Template
+     * 
+     * 
+    */
+
+    get_header(); 
+  
+?>
+
+<div class="container mt-3">
+    <h1><?php the_title(); ?></h1>
+</div>
+
+<section class="neuro-section">
+    <div class="container">
+        <div class="ratio ratio-16x9">
+    <iframe src="https://www.youtube.com/embed/PNQTfltxo4A?si=1W1_MaswvVwMrhY9" title="YouTube video player" allowfullscreen></iframe>
+    </div>
+    </div>  
+
+<section class="neuro-section">
+    <div class="container text-dark">  
+        <?php the_content(); ?>
+    </div>
+</section>
+
+<section class="neuro-section">
+<div class="container text-center">
+<div class="accordion" id="accordionExample">
+  <div class="accordion-item">
+    <h2 class="accordion-header" id="headingOne">
+      <button class="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
+      What if I don’t feel safe approaching someone?
+      </button>
+    </h2>
+    <div id="collapseOne" class="accordion-collapse collapse show" aria-labelledby="headingOne" data-bs-parent="#accordionExample">
+      <div class="accordion-body">
+      Trust your instincts. If you don't feel safe, do not approach a stranger. You don’t have to do it alone either. If you don’t feel confident speaking to a person yourself, you can speak to a passer-by or if you are at a train station, find a member of staff or police officer.
+
+It’s important to remember that if you’re concerned for your safety or the safety of someone else at any time, call 999.
+      </div>
+    </div>
+  </div>
+  <div class="accordion-item">
+    <h2 class="accordion-header" id="headingTwo">
+      <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
+      What if I don’t get a response?
+      </button>
+    </h2>
+    <div id="collapseTwo" class="accordion-collapse collapse" aria-labelledby="headingTwo" data-bs-parent="#accordionExample">
+      <div class="accordion-body">
+      It can take a while for someone who is having difficult thoughts to realise they are being spoken to. It might seem like they’re not listening. Be patient; stay in their line of sight if you can and just let them know that you’re there. When you feel ready, you can try and talk to them again.
+      </div>
+    </div>
+  </div>
+  <div class="accordion-item">
+    <h2 class="accordion-header" id="headingThree">
+      <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
+      What if they don’t want to talk to me?
+      </button>
+    </h2>
+    <div id="collapseThree" class="accordion-collapse collapse" aria-labelledby="headingThree" data-bs-parent="#accordionExample">
+      <div class="accordion-body">
+      If the person acts defensive or looks uncomfortable, it's best to alert someone to help. There is no right or wrong way to approach someone, and you don’t need special training to help – you just need to start a conversation. Simply talking to someone and interrupting their thoughts may be all it takes to encourage them to seek support.
+      </div>
+    </div>
+  </div>
+  <div class="accordion-item">
+    <h2 class="accordion-header" id="headingFour">
+      <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseFour" aria-expanded="false" aria-controls="collapseFour">
+      What if the person I approach is OK and not suicidal?
+      </button>
+    </h2>
+    <div id="collapseFour" class="accordion-collapse collapse" aria-labelledby="headingFour" data-bs-parent="#accordionExample">
+      <div class="accordion-body">
+      It’s always better to approach someone you’re worried about. Making small talk and starting a conversation is a simple way to approach someone and find out if they might need help.
+      </div>
+    </div>
+  </div>
+  <div class="accordion-item">
+    <h2 class="accordion-header" id="headingFive">
+      <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseFive" aria-expanded="false" aria-controls="collapseFive">
+      What if I make things worse?
+      </button>
+    </h2>
+    <div id="collapseFive" class="accordion-collapse collapse" aria-labelledby="headingFive" data-bs-parent="#accordionExample">
+      <div class="accordion-body">
+      There is no evidence that intervening when someone is at risk will make the situation worse and there’s no perfect way to make an intervention, just do your best. By simply being there and showing you care, you could make a difference even if they don’t respond right away.
+      </div>
+    </div>
+  </div>
+</div>
+</div>
+</section>
+
+<section class="neuro-section">
+    <div class="container text-center">
+        <h2>Remember...</h2>
+    <div class="row">
+            <div class="col-md-6 mb-4">
+                <div class="card neuro-card">
+                    <div class="card-body text-light">
+                    <h2 class="card-title">Trust your instincts</h2>                        
+                        <p class="card-text">There is no right or wrong way to approach someone and you don’t need special training to help – you just need to start a simple conversation. This is something you probably do every day without even thinking about it.</p>                                
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-6 mb-4">
+                <div class="card neuro-card">
+                    <div class="card-body text-light">
+                    <h2 class="card-title">You won’t make things worse</h2>                        
+                        <p class="card-text">There is no evidence that intervening when someone is at risk will make the situation worse and there’s no perfect way to make an intervention, just do your best. By simply being there and showing you care, you could make a difference even if they don’t respond right away.</p>
+                        
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-6 mb-4">
+                <div class="card neuro-card">
+                    <div class="card-body text-light">
+                    <h2 class="card-title">Suicide can be prevented</h2>                        
+                        <p class="card-text">Suicide can be prevented and suicidal thoughts are often temporary and can be interrupted. By starting a conversation, you could help save someone’s life and put them on a path to recovery.</p>
+                        
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-6 mb-4">
+                <div class="card neuro-card">
+                    <div class="card-body text-light">
+                    <h2 class="card-title">You’re not alone</h2>                        
+                        <p class="card-text">If you don’t feel confident speaking to a person yourself, you can speak to a passer-by, or if you are at a train station, find a member of staff or police officer. In an emergency, call 999.</p>
+                        
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+
+<section class="neuro-section">
+    <div class="container text-center">
+    <div class="row">
+        <div class="col-md-12">
+            <div class="card neuro-card">
+                <div class="card-body text-light">
+                                      
+                    <p class="card-text">If you think someone might need help, trust your instincts and if you feel safe to do so start a simple conversation. You could save a life.</p>
+                </div>
+            </div>
+        </div>            
+    </div>
+</section>
+
+<div class="container text-center text-dark">
+    <p>This page is built using resources from <a href="https://www.samaritans.org/support-us/campaign/small-talk-saves-lives/how-to-start-a-conversation/" target="_blank">Samaritan's How to start a conversation</a></p>
+</div>
+
+<?php get_footer(); ?>
