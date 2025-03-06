cfb97771c298131ff59073f49fbc3cc756d86313
Rich Howell <r.howell@nhs.net>
2025-03-06 15:27:32 +0000

    Create page-women-day.php

---
+++ page-templates/page-women-day.php
@@ -0,0 +1,202 @@
+<?php 
+
+    /** 
+     * Template Name: Women's Day Page Template
+     * 
+     * 
+    */
+
+    get_header(); 
+  
+?>
+
+<section class="neuro-section">
+    <div class="container text-center">
+        <h1>International Women's Day</h1>
+        <div class="row neuro-header-content align-items-center">            
+            <div class="col-12">
+                <p class="text-dark">IWD is a specific day dedicated to the advancement of women worldwide.</p>
+
+                <p class="text-dark">Marked by millions of people on March 8, IWD celebrates the social, economic, cultural, and political achievements of women. The day also marks a call to action for accelerating gender equality.</p>
+
+                <p class="text-dark">IWD provides an important platform to engage with communities, reflect on progress, and commit to positive action. Participation in IWD ensures progress toward equality remains on the global agenda and positive action prevails.</p>
+
+                <p class="text-dark">IWD has a long and powerful history of collective action, with many groups, countries, and bodies having actively contributed to its evolution. Today, IWD is a highly visible worldwide occurrence with significant mainstream awareness, marked by groups worldwide in a wide variety of ways.</p>
+            </div>
+        </div>
+    </div>
+</section>
+
+<section>
+    <div class="container text-center">        
+        <div class="my-5 ratio ratio-16x9">
+            <iframe src="https://www.youtube.com/embed/53ynGOaVFec?si=uieJ-tbuYxEb2lLc" allowfullscreen></iframe>
+        </div>
+    </div>
+</section>
+
+<section class="neuro-section">
+    <div class="container text-center">  
+        <h2>2025 THEME</h2>
+        <h3>Accelerate Action</h3>
+        <p class="text-muted">Collectively, we can <STRONG>Accelerate Action</strong> for gender equality.</p>
+        <p class="text-muted">Step forward in solidarity for International Women's Day (IWD) 2025 on March 8 to help #AccelerateAction.</p>
+        <p class="text-muted">At the current rate of progress, it will take until 2158, which is roughly five generations from now, to reach full gender parity, according to data from the World Economic Forum.</p>
+        <p class="text-muted">Focusing on the need to Accelerate Action emphasizes the importance of taking swift and decisive steps to achieve gender equality. It calls for increased momentum and urgency in addressing the systemic barriers and biases that women face, both in personal and professional spheres.</p>
+        <p class="text-muted">So, together, let's Accelerate Action to speed up the rate of progress worldwide.</p>
+            <div class="row mt-2">
+            <div class="col-md-6">
+                <div class="card neuro-card">
+                    <div class="card-body  text-light">
+                        <h2 class="card-title">Support the Supporters</h2>
+                        <p class="card-text">And one of the biggest ways to help Accelerate Action for gender equality is to Support the Supporters.</p> 
+
+                        <p class="card-text">With so many impactful groups across the world delivering critical work that assists women and girls, we can all lend our support in solidarity. </p>
+
+                        <p class="card-text">That's why for 2025, IWD sees a big call-to-action for all IWD events to incorporate an element of women-focused fundraising. </p>
+                        </p>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-6">
+                <div class="card neuro-card">
+                    <div class="card-body text-light">
+                        <h2 class="card-title">Everyone everywhere can Accelerate Action</h2>
+                        <p class="card-text">Remember, all IWD is activity is valid.
+
+                        <p class="card-text">That's what makes IWD so inclusive, not exclusive.</p>
+
+                        <p class="card-text">IWD remains an important opportunity for elevating and amplifying the focus on gender equality.</p>
+
+                        <p class="card-text">As such, IWD belongs to all groups, everywhere.</p>
+                    </div>
+                </div>
+            </div>
+            </div>
+            <div class="row mt-2">
+            <div class="col-md-6">
+                <div class="card neuro-card">
+                    <div class="card-body text-light">
+                        <h2 class="card-title">Strike the #AccelerateAction pose</h2>
+                        <p class="card-text">Are you in? Will you help Accelerate Action?
+
+                        <p class="card-text">Strike the #AccelerateAction pose to show solidarity. </p>
+
+                        <p class="card-text">Encourage others to help accelerate action within their own sphere of influence by sharing your #AccelerateAction image and statement across social media throughout the year using #IWD2025 #AccelerateAction.</p>
+
+                        <p class="card-text">Submit your #AccelerateAction image and statement <a href="https://www.internationalwomensday.com/login">here</a>.</p>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-6">
+                <div class="card neuro-card">
+                    <div class="card-body text-light">
+                        <h2 class="card-title">Build on IWD's century-plus history</h2>
+                        
+                        <p class="card-text">With well over a century of history and change, the first International Women's Day (IWD) was held in March 1911.</p>
+
+Read more about the IWD timeline <a href="https://www.internationalwomensday.com/Timeline">here</a>.
+                        </ul>                  
+                    </div>
+                </div>
+            </div>
+            </div>
+            <div class="row mt-2">
+            <div class="col-md-12">
+                <div class="card neuro-card">
+                    <div class="card-body text-light">
+                        <h2 class="card-title">Elevate your commitment to Accelerate Action</h2>
+                        
+                        <p class="card-text">Send in your #AccelerateAction image and statement to potentially be featured in the IWD Gallery and across the IWD social media feeds throughout the year.</p>
+                                        
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+
+<section class="neuro-section">
+    <div class="container text-center">
+    <div class="row mt-2">
+            <div class="col-md-12">
+                <div class="card neuro-card">
+                    <div class="card-body text-light">
+                        <h2 class="card-title">How can you help Accelerate Action?</h2>
+                        
+                        <p class="card-text">As individuals, we can all take steps in our daily lives to positively impact women's advancement.</p>
+
+                        <p class="card-text">We can call out stereotypes, challenge discrimination, question bias, celebrate women's success, and so much more. Additionally, sharing our knowledge and encouragement with others is key.</p>
+
+                        <p class="card-text">Impactful organisations and groups across the world deliver an array of effective strategies, resources, and activity that help #AccelerateAction in many areas including:</p>
+
+                        <ul class="list-unstyled">
+                            <li>forging women's economic empowerment</li>
+                            <li>recruiting, retaining and developing diverse talent</li>
+                            <li>supporting women and girls into leadership, decision-making, business and STEM</li>
+                            <li>designing and building infrastructure meeting the needs of women and girls</li>
+                            <li>helping women and girls make informed decisions about their health</li>
+                            <li>involving women and girls in sustainable agriculture and food security</li>
+                            <li>providing women and girls with access to quality education and training</li>
+                            <li>elevating women and girl's participation and achievement in sport</li>
+                            <li>promoting creative and artistic talent of women and girls</li>
+                            <li>addressing further areas supporting the advancement of women and girls</li>
+                        </ul>
+                                        
+                    </div>
+                </div>
+            </div>
+        </div>
+</div>
+</section>
+
+
+<section class="neuro-section">
+    <div class="container text-center">  
+        <h2>RESOURCES</h2>
+            <div class="row mt-2">
+            <div class="col-md-6">
+                <div class="card neuro-card">
+                    <div class="card-body  text-light">
+                        <h2 class="card-title">Email Signatures</h2>
+                        <a class="btn btn-primary" target="_blank" href="https://www.internationalwomensday.com/EmailSignature">Download</a>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-6">
+                <div class="card neuro-card">
+                    <div class="card-body text-light">
+                        <h2 class="card-title">Awareness Posters</h2>
+                        <a class="btn btn-primary" target="_blank" href="https://www.internationalwomensday.com/IWD-Posters">Download</a>
+                    </div>
+                </div>
+            </div>
+            </div>
+            <div class="row mt-2">
+            <div class="col-md-6">
+                <div class="card neuro-card">
+                    <div class="card-body text-light">
+                        <h2 class="card-title">Social Media Selfie Cards</h2>
+                        <a class="btn btn-primary" target="_blank" href="https://www.internationalwomensday.com/SocialMedia-Cards">Download</a>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-6">
+                <div class="card neuro-card">
+                    <div class="card-body text-light">
+                        <h2 class="card-title">Teams Backgrounds</h2>
+                        <a class="btn btn-primary" target="_blank" href="https://www.internationalwomensday.com/Zoom-backgrounds">Download</a>              
+                    </div>
+                </div>
+            </div>
+            </div>            
+        </div>
+    </div>
+</section>
+
+
+<div class="container text-center">
+    <p>This page is built using resources from <a href="https://www.internationalwomensday.com/" target="_blank">International Women's Day</a></p>
+</div>
+
+<?php get_footer(); ?>
