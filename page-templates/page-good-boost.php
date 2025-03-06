e7d990c8aa0774474a386e15c28c1460f9b5b0a5
Rich Howell <r.howell@nhs.net>
2025-03-06 15:26:44 +0000

    Good Boost

---
+++ page-templates/page-good-boost.php
@@ -0,0 +1,163 @@
+<?php 
+
+    /** 
+     * Template Name: Good Boost Page Template
+     * 
+     * 
+    */
+
+    get_header(); 
+  
+?>
+
+<!-- OLD --> 
+
+<section class="neuro-section">
+    <div class="container text-center">
+        <img src="<?php echo get_bloginfo('template_directory'); ?>/img/good-boost-logo.png" alt="Good Boost Logo">
+        <div class="row neuro-header-content align-items-center">            
+            <div class="col-12">
+                <p class="text-dark">Good Boost is a multi award-winning social enterprise working with community, leisure and health teams to deliver <strong>therapeutic exercise</strong> and <strong>self-management support in the heart of the community</strong></p>
+            </div>
+        </div>
+    </div>
+</section>
+
+<section class="neuro-section">
+    <div class="container text-center">
+<div class="row">
+    <div class="col-md-3">
+        <div class="card">
+
+            <div class="card-body">
+                <h5 class="card-title">Land Exercise</h5>
+                <p class="card-text">Good Boost is a multi award-winning social enterprise working with community, leisure and health teams to deliver therapeutic exercise and self-management support in the heart of the community.</p>
+            </div>
+        </div>
+    </div>
+    <div class="col-md-3">
+        <div class="card">
+
+            <div class="card-body">
+                <h5 class="card-title">Aqua Fitness</h5>
+                <p class="card-text">Good Boost is a multi award-winning social enterprise working with community, leisure and health teams to deliver therapeutic exercise and self-management support in the heart of the community.</p>
+            </div>
+        </div>
+    </div>
+    <div class="col-md-3">
+        <div class="card">
+
+            <div class="card-body">
+                <h5 class="card-title">Aqua Rehab</h5>
+                <p class="card-text">Good Boost is a multi award-winning social enterprise working with community, leisure and health teams to deliver therapeutic exercise and self-management support in the heart of the community.</p>
+            </div>
+        </div>
+    </div>
+    <div class="col-md-3">
+        <div class="card">
+
+            <div class="card-body">
+                <h5 class="card-title">Aqua Natal</h5>
+                <p class="card-text">Good Boost is a multi award-winning social enterprise working with community, leisure and health teams to deliver therapeutic exercise and self-management support in the heart of the community.</p>
+            </div>
+        </div>
+    </div>
+</div>    
+</section>
+
+<section class="neuro-section">
+    <div class="container text-center">
+        <div class="row">
+            <div class="col-12">
+                <div class="card">
+
+                    <div class="card-body">
+                        <h5 class="card-title">Virtual Group Classes</h5>
+                        <p class="card-text">Good Boost is a multi award-winning social enterprise working with community, leisure and health teams to deliver therapeutic exercise and self-management support in the heart of the community.</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+
+<section class="neuro-section">
+    <div class="container text-dark">
+        <div class="row">
+            <div class="col-12">                                    
+                <ul>
+                    <li>Good Boost provides both Land and Aquatic exercises programmes.</li>
+                    <li>Low-level, rehab-focused exercises, designed to improve function and symptoms at a particular body area (lower-back, shoulder,hip, knee).
+                        Participants can also choose to work on a rehab aim such as improving balance, strength or cardio vascular fitness.</li>
+                        <li>Aqua-natal programmes are also available through the BumpBoost programmes.</li>
+                        <li>While each participant has their own tailored exercise programme, Good Boost is delivered in a group class with the support and motivation of a facilitator. Participants feel connected and supported, addressing feelings of social isolation and having fun along the way.></li>                        
+                </ul>
+            </div>
+        </div>
+    </div>
+</section>
+
+
+<section class="neuro-section">
+    <div class="container text-dark">
+        <div class="row">
+            <div class="col-12">
+                <h3>Who is Good Boost for?</h3>
+                <ul>                   
+                    <li>Classes are designed specifically for people living with musculoskeletal conditions and concurrent health problems and anyone wanting a gentle road back into fitness</li>
+                    <li>Exercises are delivered through tablet computers using good boosts unique AI technology to generate bespoke exercise programmes</li>
+                    <li>Each participants' health, symptoms and functional ability are considered to create paced, achievable exercise programmes, building confidence with exercise and physical activity</li>
+                    <li>Our Aqua programme consider water confidence so appropriate for non-swimmers and can be a great way to build confidence in the water</li>
+                    <li>Land based programmes can be seated in supported standing for those with balance or weight bearing difficulties</li>
+                </ul>
+            </div>
+        </div>
+    </div>
+</section>
+
+<section class="neuro-section">
+    <div class="container text-center">
+        <div class="row">
+            <div class="col-12">
+                <div class="card">
+                    <div class="card-body">
+                        <span style="font-size: 3rem">66%</span> of participants using Good Boost while waiting for healthcare shows an improvement in their symptoms*
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+
+<section class="neuro-section">
+    <div class="container text-center">
+        <div class="row">
+            <div class="col-6">
+                <div class="card">
+                    <div class="card-body">
+                        <h5>Where to find</h5>
+                        <h4>Good Boost?</h4>
+                        <a href="https://www.goodboost.ai/about-us">goodboost.ai/about-us</a>
+                    </div>
+                </div>
+            </div>
+            <div class="col-6">
+                <div class="card">
+                    <div class="card-body">
+                        <h5>Good Boost is available at:</h5>
+                        <ul class="list-unstyled">
+                            <li>Wellington Civic and Leisure Centre</li>
+                            <li>Abraham Darby Sports and Leisure Centre</li>
+                            <li>Teme Leisure Center</li>
+                            <li>Oswestry Leisure Centre</li>
+                            <li>The Quarry Swimming and Fitness Center</li>
+                        </ul>
+                        <strong>Please contact your center of choice for times and bookings</strong>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+
+<?php get_footer(); ?>
